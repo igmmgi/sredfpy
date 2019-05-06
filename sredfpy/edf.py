e27506f5503c25269371cb82397e07d6026c5595
ian <ian.mackenzie@uni-tuebingen.de>
2019-05-06 12:58:47 +0200

    added option to print messaged to file

--- sredfpy/edf.py
+++ sredfpy/edf.py
@@ -1,3 +1,5 @@
+import os
+import numpy as np
 import pandas as pd
 from sredfpy import edf_read
 
@@ -46,3 +48,17 @@ class EDF:
         self.fsample = pd.DataFrame(self.fsample)
         self.fevent = pd.DataFrame(self.fevent)
         self.rec = pd.DataFrame(self.rec)
+
+    def print_message(self, unique_only=False):
+        """ Print messages within *.edf file to txt file. """
+
+        fname = os.path.splitext(self.fname)[0]
+
+        if not unique_only:
+            with open(fname + "_messages.txt", "w") as txtfile:
+                for message in self.fevent["message"]:
+                    txtfile.write(message)
+        else:
+            with open(fname + "_unique_messages.txt", "w") as txtfile:
+                for message in np.unique(self.fevent["message"]):
+                    txtfile.write(message)
